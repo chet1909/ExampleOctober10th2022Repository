0cab80a1910562c7113f9f7c4d6b42264311f69a
chet1909 <reddychethan858@gmail.com>
2023-01-02 17:55:21 +0530

    Odd Number1to15

---
+++ Web-Automation/src/com/sgtesting/p2/DisplayOdd0to15.java
@@ -0,0 +1,17 @@
+package com.sgtesting.p2;
+
+public class DisplayOdd0to15 {
+
+	public static void main(String[] args) {
+		int i=0;
+		while(i<=15) {
+			if(i%2!=0) {
+				System.out.println("odd numbers"+i);
+				
+			}
+			i++;
+	}
+	}
+}
+
+
