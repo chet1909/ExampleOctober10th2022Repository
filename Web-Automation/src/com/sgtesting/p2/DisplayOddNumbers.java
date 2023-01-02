e4ece84e8f4f6c13a3e7c1f591a757f15a250911
chet1909 <reddychethan858@gmail.com>
2023-01-02 17:44:35 +0530

    Odd Numbers Demo

---
+++ Web-Automation/src/com/sgtesting/p2/DisplayOddNumbers.java
@@ -0,0 +1,18 @@
+package com.sgtesting.p2;
+
+public class DisplayOddNumbers {
+
+	public static void main(String[] args) {
+
+		int i=0;
+		while(i<=20) {
+			if(i%2!=0) {
+				System.out.println("odd numbers"+i);
+				
+			}
+			i++;
+		}
+			
+	}
+
+}
