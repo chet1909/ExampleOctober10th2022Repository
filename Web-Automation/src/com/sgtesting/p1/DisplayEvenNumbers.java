69a6815065bab5f2b86098df38b4d48c91eaa8ec
chet1909 <reddychethan858@gmail.com>
2023-01-02 17:25:52 +0530

    EvenNumbersDemo

---
+++ Web-Automation/src/com/sgtesting/p1/DisplayEvenNumbers.java
@@ -0,0 +1,20 @@
+package com.sgtesting.p1;
+
+public class DisplayEvenNumbers {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for(int i=0;i<=20;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("Even Numbers"+i);
+
+			}
+			System.out.println();
+		}
+	}
+
+}
